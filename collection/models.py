3d6bdce55ae74f7c82f3989f393708132ccf5259
beno <19noname19@gmail.com>
2013-12-01 16:01:09 +0100

    add image field to image model and fix upload_to url

--- collection/models.py
+++ collection/models.py
@@ -3,6 +3,10 @@ from autoslug import AutoSlugField
 
 from section.models import Section
 
+def image_fancy_path(instance, filename):
+    return 'images/%s/' % self.instance.collection.slug
+
+
 
 class Collection(models.Model):
     title = models.CharField(verbose_name=u'Collection title', max_length=50, blank=False, null=False, unique=True)
@@ -25,6 +29,7 @@ class CollectionImage(models.Model):
     title = models.CharField(verbose_name=u'Image title', max_length=30, blank=False, null=False, unique=True)
     slug = AutoSlugField(populate_from='title')
     description = models.TextField(verbose_name=u'Image description')
+    image = models.ImageField(verbose_name='Image', upload_to=image_fancy_path)
 
     def __unicode__(self):
         return (u"%s") % (self.title)
