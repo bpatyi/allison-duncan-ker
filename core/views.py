21192b61d6b15da8e37f57d0242f3566c716491b
beno <19noname19@gmail.com>
2013-12-01 17:41:23 +0100

    create index view

--- core/views.py
+++ core/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template.response import TemplateResponse
+from django.core.exceptions import ValidationError
+from django.core.urlresolvers import reverse
+from django.views.defaults import page_not_found
+from django.shortcuts import render_to_response, get_object_or_404, redirect
+
+from django.views.generic import View
+
+
+class Index(View):
+    template_name = 'index.html'
+
+    def get(self, request, *args, **kwargs):
+
+        context = {}
+
+        return TemplateResponse(request, self.template_name, context)
+
