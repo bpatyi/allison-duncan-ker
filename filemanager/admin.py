3d1ba75a37fb5d569df6c970a5c04577bd989d53
beno <19noname19@gmail.com>
2013-12-01 16:00:30 +0100

    create file model and filemanager admin

--- filemanager/admin.py
+++ filemanager/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from filemanager.models import File
+
+class FileAdmin(admin.ModelAdmin):
+    model = File
+    list_display = ('pk', 'the_file', 'description')
+
+admin.site.register(File, FileAdmin)
